5e39609cd4b6adc57b948c4adc2339804f36b70e
wuchuan <1224347332@qq.com>
2018-06-03 23:48:42 +0800

    Site updated: 2018-06-03 23:48:42

--- js/script.js
+++ js/script.js
@@ -1,76 +1,103 @@
 (function ($) {
-    $('.navbar-burger').click(function () {
-        $(this).toggleClass('is-active');
-        $('.navbar-main .navbar-start').toggleClass('is-active');
-        $('.navbar-main .navbar-end').toggleClass('is-active');
-    });
+  console.log('© Theme-Vexo | https://github.com/yanm1ng/hexo-theme-vexo')
+  var app = $('.app-body')
+  var header = $('.header')
+  var banner = document.getElementById('article-banner') || false
+  var about = document.getElementById('about-banner') || false
+  var top = $('.scroll-top')
+  var catalog = $('.catalog-container .toc-main')
+  var isOpen = false
 
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('.navbar-main').outerHeight();
+  $(document).ready(function () {
+    NProgress.start()
+    $('#nprogress .bar').css({
+      'background': '#42b983'
+    })
+    $('#nprogress .spinner').hide()
 
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta) {
-            return;
-        }
-
-        // If they scrolled down and are past the navbar, add class .navbar-down.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight) {
-            var posY = Math.min(st, navbarHeight);
-            // Scroll Down
-            $('.navbar-main').css({
-                '-webkit-transform' : 'translateY(-' + posY + 'px)',
-                '-moz-transform'    : 'translateY(-' + posY + 'px)',
-                '-ms-transform'     : 'translateY(-' + posY + 'px)',
-                '-o-transform'      : 'translateY(-' + posY + 'px)',
-                'transform'         : 'translateY(-' + posY + 'px)'
-            });
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {
-                $('.navbar-main').css({
-                    '-webkit-transform' : 'translateY(0px)',
-                    '-moz-transform'    : 'translateY(0px)',
-                    '-ms-transform'     : 'translateY(0px)',
-                    '-o-transform'      : 'translateY(0px)',
-                    'transform'         : 'translateY(0px)'
-                });
-            }
-        }
-
-        lastScrollTop = st;
+    var fade = {
+      transform: 'translateY(0)',
+      opacity: 1
     }
+    if (banner) {
+      app.css('transition-delay', '0.15s')
+      $('#article-banner').children().css(fade)
+    }
+    if (about) {
+      $('.author').children().css(fade)
+    }
+    app.css(fade)
+  })
 
-    $('.article.gallery img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="caption">' + this.alt + '</div>');
-            }
-        }
-    });
+  window.onload = function () {
+    setTimeout(function () {
+      NProgress.done()
+    }, 200)
+  }
 
-    $('.article-entry').find('h1, h2, h3, h4, h5, h6').on('click', function () {
-        if ($(this).get(0).id) {
-            window.location.hash = $(this).get(0).id;
-        }
-    });
-})(jQuery);
+  $('.menu').on('click', function () {
+    if (!header.hasClass('fixed-header') || isOpen) {
+      header.toggleClass('fixed-header')
+      isOpen = !isOpen
+    }
+    $('.menu-mask').toggleClass('open')
+  })
+
+  $('#tag-cloud a').on('click', function () {
+    var list = $('.tag-list')
+    var name = $(this).data('name')
+    var maoH = list.find('#' + name).offset().top
+
+    $('html,body').animate({
+      scrollTop: maoH - header.height()
+    }, 500)
+  })
+
+  $('.reward-btn').on('click', function () {
+    $('.money-code').fadeToggle()
+  })
+
+  $('.arrow-down').on('click', function () {
+    $('html, body').animate({
+      scrollTop: banner.offsetHeight - header.height()
+    }, 500)
+  })
+
+  $('.toc-nav a').on('click', function (e) {
+    e.preventDefault()
+    var catalogTarget = e.currentTarget
+    var scrollTarget = $(catalogTarget.getAttribute('href'))
+    var top = scrollTarget.offset().top
+    if (top > 0) {
+      $('html,body').animate({
+        scrollTop: top - 65
+      }, 500)
+    }
+  })
+
+  top.on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600)
+  })
+
+  document.addEventListener('scroll', function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    var headerH = header.height()
+    if (banner) {
+      if (scrollTop > headerH) {
+        header.addClass('fixed-header')
+      } else if (scrollTop === 0) {
+        header.removeClass('fixed-header')
+      }
+    }
+    if (scrollTop > 100) {
+      top.addClass('opacity')
+    } else {
+      top.removeClass('opacity')
+    }
+    if (scrollTop > 190) {
+      catalog.addClass('fixed-toc')
+    } else {
+      catalog.removeClass('fixed-toc')
+    }
+  })
+})(jQuery)
